ac84b81327e06921fde46b590fcfeaf6e044559e
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-10-03 15:08:12 +0200

    feat: move points with keyboard or mouse

--- camera_dewarping/camera.py
+++ camera_dewarping/camera.py
@@ -2,9 +2,15 @@ import cv2
 import numpy as np
 import random
 
-class Params():
+class KeyCodes():
     NONE = 255
     EXIT = ord("q")
+    LEFT = ord("a")
+    RIGHT = ord("d")
+    UP = ord("w")
+    DOWN = ord("s")
+    PLUS = 43
+    MINUS = 45
 
 class Dewarping:
     def __init__(self):
@@ -17,18 +23,77 @@ class Dewarping:
         ]
         self.points = {0: [], 1: [], 2: []}
         self.masks = {0: [], 1: [], 2: []}
-        self.transforms = {0: [], 1: [], 2: []}
-        self.last_points = {0: 0, 1: 0, 2: 0}
         self.cap = None
+        self.selected_point = None
+        self.movement_offset = 50
+        self.point_threshold = 50
+        self.btn_down = False
+        self.mouse_moved = False
+
+    def update_point(self, p, offset_x, offset_y):
+
+        new_point = (p[0] + offset_x, p[1] + offset_y)
+
+        index = None
+        for i, point in enumerate(self.points[self.current_source]):
+            if point == p:
+                index = i
+                break
+        
+        if index is not None:
+            self.points[self.current_source][index] = new_point
+        
+        index_mask = None
+        for i, mask in enumerate(self.masks[self.current_source]):
+            index_point = None
+            for j, point in enumerate(mask):
+                if point[0] == p[0] and point[1] == p[1]:
+                    index_mask = i
+                    index_point = j
+                    break
+            if index_mask is not None and index_point is not None:
+                self.masks[self.current_source][index_mask][index_point] = new_point
+
+        self.selected_point = new_point
+            
 
     def show(self, frame):
         frame_downscaled = cv2.resize(frame, (0, 0), fx=self.scale, fy=self.scale)
         cv2.imshow("preview", frame_downscaled)
-        return cv2.waitKey(1) & 0xFF
+        key_pressed = cv2.waitKey(1)
+        return key_pressed & 0xFF
     
-    def on_mouse(self, event, x, y, flags, param):
+    def on_mouse(self, event, x, y, flags, data):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self.points[self.current_source].append((int(x // self.scale), int(y // self.scale)))
+            new_point = True
+            selected_point = None
+            self.btn_down = True
+            # check if x,y is close to any of the points
+            for point in self.points[self.current_source]:
+                if abs(point[0] - x // self.scale) < self.point_threshold and abs(point[1] - y // self.scale) < self.point_threshold:
+                    new_point = False
+                    selected_point = point
+                    break
+            
+            if new_point:
+                self.points[self.current_source].append((int(x // self.scale), int(y // self.scale)))
+                self.selected_point = self.points[self.current_source][-1]
+            elif selected_point is not None:
+                self.selected_point = selected_point
+
+        elif event == cv2.EVENT_MOUSEMOVE and self.btn_down:
+            self.mouse_moved = True
+            x = max(0, min(x, self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+            y = max(0, min(y, self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+            offset_x = int(x // self.scale) - self.selected_point[0]
+            offset_y = int(y // self.scale) - self.selected_point[1]
+
+
+            self.update_point(self.selected_point, offset_x, offset_y)
+
+        elif event == cv2.EVENT_LBUTTONUP and self.btn_down:
+            self.btn_down = False
+
     
     def crop(self, frame):
         match self.current_source:
@@ -40,6 +105,11 @@ class Dewarping:
                 return frame[:, 1400:2700]
     
     def get_triangles(self, frame):
+        for point in self.points[self.current_source]:
+            if point == self.selected_point:
+                cv2.circle(frame, point, 15, (0, 0, 255), -1)
+            else:
+                cv2.circle(frame, point, 15, (0, 255, 0), -1)
         if len(self.points[self.current_source]) > 3:
 
             tri = cv2.Subdiv2D((0, 0, frame.shape[1], frame.shape[0]))
@@ -63,6 +133,8 @@ class Dewarping:
 
                 self.masks[self.current_source].append(np.array([pt1, pt2, pt3]))
 
+        return frame
+
     def draw_masks(self, frame, index=None):
         image_with_masks = np.zeros_like(frame)
         for i,mask in enumerate(self.masks[self.current_source]):
@@ -73,16 +145,36 @@ class Dewarping:
             image_with_masks = cv2.bitwise_or(image_with_masks, overlay)
 
         return image_with_masks
+    
+    def add_text(self, frame, text, x, y):
+        frame = cv2.putText(frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 12)
+        frame = cv2.putText(frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 6)
+        return frame
+    
+    def add_info(self, frame):
+        frame = self.add_text(frame, "Movement offset: " + str(self.movement_offset), 50, 100)
+        return frame
 
     def on_key(self, key, overlay_1, overlay_2):
-        if key == Params.EXIT:
+        if key == KeyCodes.EXIT:
             return False
         elif key >= ord("1") and key <= ord("3"):
             self.current_source = key - ord("1")
             self.cap = cv2.VideoCapture(self.videos[self.current_source])
-        elif key == ord("s"):
-            cv2.imwrite("output/0.jpg", overlay_1)
-            cv2.imwrite("output/1.jpg", overlay_2)
+        elif key == KeyCodes.PLUS:
+            self.movement_offset += 5
+        elif key == KeyCodes.MINUS:
+            self.movement_offset -= 5
+
+        if self.selected_point is not None:
+            if key == KeyCodes.LEFT:
+                self.update_point(self.selected_point, -self.movement_offset, 0)
+            elif key == KeyCodes.RIGHT:
+                self.update_point(self.selected_point, self.movement_offset, 0)
+            elif key == KeyCodes.UP:
+                self.update_point(self.selected_point, 0, -self.movement_offset)
+            elif key == KeyCodes.DOWN:
+                self.update_point(self.selected_point, 0, self.movement_offset)
         return True
 
     def render(self):
@@ -100,7 +192,8 @@ class Dewarping:
 
             cv2.setMouseCallback("preview", self.on_mouse)
             frame = self.crop(frame)
-            self.get_triangles(frame)
+            frame = self.get_triangles(frame)
+            frame = self.add_info(frame)
 
             overlay_1 = self.draw_masks(frame, 0)
             overlay_2 = self.draw_masks(frame, 1)
