16ee87d6092f58f3311c91a603ae7cefdd1f38f3
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-10-23 23:15:23 +0200

    chore: somewhat something works but not everything

--- camera_dewarping/camera.py
+++ camera_dewarping/camera.py
@@ -27,34 +27,38 @@ class Dewarping:
             "assets/right.mp4",
             "assets/center.mp4",
         ]
+        self.old_points = {0: [], 1: [], 2: []}
         self.points = {0: [], 1: [], 2: []}
-        self.old_points = []
-        self.warped_points = []
+        self.old_triangles = {0: [], 1: [], 2: []}
         self.triangles = {0: [], 1: [], 2: []}
         self.cap = None
         self.selected_point = None
-        self.movement_offset = 50
         self.point_threshold = 50
         self.btn_down = False
         self.warping = False
         self.background = None
         self.overlay = None
         self.circles = None
+        self.lines = None
+        self.old_circles = None
+        self.old_lines = None
 
     def update_point(self, p, offset_x, offset_y, warp=False):
+        points = self.old_points[self.current_source] if not warp else self.points[self.current_source]
+        triangles = self.old_triangles[self.current_source] if not warp else self.triangles[self.current_source]
         new_point = (p[0] + offset_x, p[1] + offset_y)
 
         index = None
-        for i, point in enumerate(self.points[self.current_source]):
+        for i, point in enumerate(points):
             if point == p:
                 index = i
                 break
 
         if index is not None:
-            self.points[self.current_source][index] = new_point
+            points[index] = new_point
 
         index_mask = None
-        for i, mask in enumerate(self.triangles[self.current_source]):
+        for i, mask in enumerate(triangles):
             index_point = None
             for j, point in enumerate(mask):
                 if point[0] == p[0] and point[1] == p[1]:
@@ -62,23 +66,47 @@ class Dewarping:
                     index_point = j
                     break
             if index_mask is not None and index_point is not None:
-                self.triangles[self.current_source][index_mask][index_point] = new_point
+                triangles[index_mask][index_point] = new_point
 
         self.selected_point = new_point
 
+    def _add_layer(self, frame, layer):
+        if frame.shape != layer.shape:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        mask = 255 - layer[:, :, 3]
+        frame = cv2.bitwise_and(frame, frame, mask=mask)
+        frame = cv2.bitwise_or(frame, layer)
+        return frame
+
     def show(self, frame):
+        self.draw_circles(frame, warp=False)
+        self.draw_lines(frame, warp=False)
+        if self.old_circles is not None:
+            frame = self._add_layer(frame, self.old_circles)
+        if self.old_lines is not None:
+            frame = self._add_layer(frame, self.old_lines)
+
+        if self.warping:
+            self.draw_circles(frame, warp=True)
+            # self.draw_lines(frame, warp=self.warping)
+            if self.circles is not None:
+                frame = self._add_layer(frame, self.circles)
+            # if self.lines is not None:
+                # frame = self._add_layer(frame, self.lines)
+        
         frame_downscaled = cv2.resize(frame, (0, 0), fx=self.scale, fy=self.scale)
         cv2.imshow("preview", frame_downscaled)
         key_pressed = cv2.waitKey(1)
         return key_pressed & 0xFF
 
-    def _select_point(self, x, y):
+    def _select_point(self, x, y, warp=False):
         new_point = True
         selected_point = None
         self.btn_down = True
+        points = self.old_points[self.current_source] if not warp else self.points[self.current_source]
 
         # check if x,y is close to any of the points
-        for point in self.points[self.current_source]:
+        for point in points:
             if (
                 abs(point[0] - x) < self.point_threshold
                 and abs(point[1] - y) < self.point_threshold
@@ -86,63 +114,103 @@ class Dewarping:
                 new_point = False
                 selected_point = point
                 break
-
+        
         if new_point:
-            self.points[self.current_source].append((int(x), int(y)))
-            self.selected_point = self.points[self.current_source][-1]
+            points.append((int(x), int(y)))
+            self.selected_point = points[-1]
         elif selected_point is not None:
             self.selected_point = selected_point
+
+
 
     def on_mouse(self, event, x, y, flags, data):
         x = x // self.scale
         y = y // self.scale
 
-        if event == cv2.EVENT_LBUTTONDOWN:
-            self._select_point(x, y)
+        if not self.warping:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self._select_point(x, y)
 
-        elif event == cv2.EVENT_MOUSEMOVE and self.btn_down:
-            x = max(0, min(x, self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
-            y = max(0, min(y, self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-            offset_x = int(x) - self.selected_point[0]
-            offset_y = int(y) - self.selected_point[1]
+            elif event == cv2.EVENT_MOUSEMOVE and self.btn_down:
+                offset_x = int(x) - self.selected_point[0]
+                offset_y = int(y) - self.selected_point[1]
 
-            self.update_point(self.selected_point, offset_x, offset_y)
+                self.update_point(self.selected_point, offset_x, offset_y)
 
-        elif event == cv2.EVENT_LBUTTONUP and self.btn_down:
-            self.btn_down = False
+            elif event == cv2.EVENT_LBUTTONUP and self.btn_down:
+                self.btn_down = False
+        else:
+            if event == cv2.EVENT_LBUTTONDOWN:
+                self._select_point(x, y, warp=True)
+
+            elif event == cv2.EVENT_MOUSEMOVE and self.btn_down:
+                offset_x = int(x) - self.selected_point[0]
+                offset_y = int(y) - self.selected_point[1]
+
+                self.update_point(self.selected_point, offset_x, offset_y, warp=True)
+            
+            elif event == cv2.EVENT_LBUTTONUP and self.btn_down:
+                self.btn_down = False
 
     def crop(self, frame):
         match self.current_source:
             case 0:
-                self.width = 1200
                 return frame[:, 1400:2600]
             case 1:
-                self.width = 1100
                 return frame[:, 1450:2550]
             case 2:
-                self.width = 1300
                 return frame[:, 1400:2700]
 
-    def get_triangles(self, frame, draw=False) -> None:
-        for point in self.points[self.current_source]:
-            if point == self.selected_point:
-                cv2.circle(frame, point, 15, (0, 0, 255, 255), -1)
-            else:
-                cv2.circle(frame, point, 15, (0, 255, 0, 255), -1)
+    def draw_circles(self, frame, warp=False):
 
-        if (
-            len(self.points[self.current_source]) > 3
-            and self.points[self.current_source] != self.old_points
-        ):
+        points = self.old_points[self.current_source] if not warp else self.points[self.current_source]
+
+        if points != []:
+            if warp:
+                self.circles = np.zeros(
+                    (frame.shape[0], frame.shape[1], 4), dtype=np.uint8
+                )
+            else:
+                self.old_circles = np.zeros(
+                    (frame.shape[0], frame.shape[1], 4), dtype=np.uint8
+                )
+
+
+        for point in self.old_points[self.current_source]:
+            if warp:
+                if point == self.selected_point:
+                    cv2.circle(self.circles, point, 15, (255, 0, 255, 255), -1)
+                else:
+                    cv2.circle(self.circles, point, 15, (0, 255, 255, 255), -1)
+            
+            else:
+                if point == self.selected_point:
+                    cv2.circle(self.old_circles, point, 15, (0, 0, 255, 255), -1)
+                else:
+                    cv2.circle(self.old_circles, point, 15, (0, 255, 0, 255), -1)
+
+    def draw_lines(self, frame, warp=False):
+        if self.old_triangles != []:
+            self.old_lines = np.zeros(
+                (frame.shape[0], frame.shape[1], 4), dtype=np.uint8
+            )
+            for triangle in self.old_triangles[self.current_source]:
+                if warp:
+                    cv2.polylines(self.lines, [triangle], True, (255, 255, 0, 255), 3)
+                else:
+                    cv2.polylines(self.old_lines, [triangle], True, (255, 0, 0, 255), 3)
+
+    def get_triangles(self, frame, draw=False) -> None:
+        if len(self.old_points[self.current_source]) > 3:
             tri = cv2.Subdiv2D((0, 0, frame.shape[1], frame.shape[0]))
             # draw triangles
-            for point in self.points[self.current_source]:
+            for point in self.old_points[self.current_source]:
                 tri.insert(point)
 
             # convert triangles to int
             triangles = tri.getTriangleList().astype(np.int32)
 
-            self.triangles[self.current_source] = []
+            self.old_triangles[self.current_source] = []
 
             # draw delaunay triangles
             for triangle in triangles:
@@ -150,13 +218,11 @@ class Dewarping:
                 pt2 = tuple(triangle[2:4])
                 pt3 = tuple(triangle[4:6])
 
-                self.triangles[self.current_source].append(np.array([pt1, pt2, pt3]))
-
-            self.old_points = deepcopy(self.points[self.current_source])
+                self.old_triangles[self.current_source].append(np.array([pt1, pt2, pt3]))
 
     def draw_masks(self, frame, index=None) -> np.ndarray:
         image_with_masks = np.zeros_like(frame)
-        for i, mask in enumerate(self.triangles[self.current_source]):
+        for i, mask in enumerate(self.old_triangles[self.current_source]):
             if index is not None and i != index:
                 continue
             overlay = cv2.fillConvexPoly(np.zeros_like(frame), mask, (255, 255, 255))
@@ -169,23 +235,14 @@ class Dewarping:
         # create transparent overlay (4 channels)
         self.overlay = np.zeros((frame.shape[0], frame.shape[1], 4), dtype=np.uint8)
         # draw triangles
-        for triangle in self.triangles[self.current_source]:
+        for triangle in self.old_triangles[self.current_source]:
             cv2.polylines(self.overlay, [triangle], True, (0, 0, 255, 128), 3)
             # cv2.fillConvexPoly(self.overlay, triangle, (255, 255, 255, 255))
-
-    def add_text(self, frame, text, x, y):
-        frame = cv2.putText(
-            frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0), 12
-        )
-        frame = cv2.putText(
-            frame, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 6
-        )
-        return frame
 
     def warp(self, frame):
         if self.warped_points == []:
             self.background = deepcopy(frame)
-            for triangle in self.triangles[self.current_source]:
+            for triangle in self.old_triangles[self.current_source]:
                 pt1, pt2, pt3 = triangle
                 self.warped_points.append(np.array([pt1, pt2, pt3]))
         #
@@ -197,11 +254,7 @@ class Dewarping:
 
         return background
 
-    def add_info(self, frame, text):
-        frame = self.add_text(frame, text, 50, 100)
-        return frame
-
-    def on_key(self, key, overlay_1, overlay_2):
+    def on_key(self, key):
         if key == KeyCodes.EXIT:
             return False
         elif ord("1") <= key <= ord("3"):
@@ -210,24 +263,29 @@ class Dewarping:
 
         if self.selected_point is not None:
             if key == KeyCodes.CANCEL:
-                self.points[self.current_source].remove(self.selected_point)
+                self.old_points[self.current_source].remove(self.selected_point)
                 self.selected_point = None
             elif key == KeyCodes.ESC:
                 self.selected_point = None
 
         if key == KeyCodes.ENTER:
-            self.selected_point = None
             self.warping = True
+            self.circles = deepcopy(self.old_circles)
+            self.lines = deepcopy(self.old_lines)
+            self.points = deepcopy(self.old_points)
+            self.triangles = deepcopy(self.old_triangles)
+            self.selected_point = None
         return True
 
     def render(self):
         cv2.namedWindow("preview")
 
         self.cap = cv2.VideoCapture(self.videos[self.current_source])
-        key, overlay_1, overlay_2 = -1, None, None
+        key = KeyCodes.NONE
 
-        while self.on_key(key, overlay_1, overlay_2):
+        while self.on_key(key):
             ret, frame = self.cap.read()
+            self.background = frame
             if not ret:
                 # restart video if it ends
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
@@ -236,12 +294,6 @@ class Dewarping:
             cv2.setMouseCallback("preview", self.on_mouse)
             frame = self.crop(frame)
             self.get_triangles(frame)
-            if self.overlay is not None:
-                pass
-                # cv2.bitwise_and(frame, self.overlay)
-            if self.warping:
-                frame = self.warp(frame)
-                # frame = np.hstack((frame, overlay))
             key = self.show(frame)
 
         self.cap.release()
